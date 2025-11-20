fba2eb054bc50024ac3f9f5aaf2d1aef90dc5d9b
AngeloAlbanesi <angeloalbanesi99@gmail.com>
2025-11-20 15:56:37 +0100

    creazione frontend pagine /profile e /events/new

--- frontend/client/src/main.jsx
+++ frontend/client/src/main.jsx
@@ -3,6 +3,7 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import { BrowserRouter } from "react-router-dom";
 import App from "./App.jsx";
+import './index.css'; // Import global styles (Tailwind)
 
 const root = document.getElementById("root");
 
@@ -14,4 +15,3 @@ ReactDOM.createRoot(root).render(
 
 
 
-  
