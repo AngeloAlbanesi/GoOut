7bd5e6b0b727d244bd18436572b3e6a706af1cc4
AngeloAlbanesi <angeloalbanesi99@gmail.com>
2025-11-24 18:15:57 +0100

    commento google auth

--- frontend/client/src/main.jsx
+++ frontend/client/src/main.jsx
@@ -3,23 +3,23 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import { BrowserRouter } from "react-router";
 import { AuthProvider } from './context/AuthContext';
-import { GoogleOAuthProvider } from '@react-oauth/google';
+//import { GoogleOAuthProvider } from '@react-oauth/google';
 import App from "./App.jsx";
 import './index.css'; // Import global styles (Tailwind)
 
-const googleClientId = "http://78021356766-pj88mf5o7ptdtbugdc8nlqu4sjjdnjpv.apps.googleusercontent.com/"
+//const googleClientId = "http://78021356766-pj88mf5o7ptdtbugdc8nlqu4sjjdnjpv.apps.googleusercontent.com/"
 const root = document.getElementById("root");
 
 
 ReactDOM.createRoot(root).render(
   <React.StrictMode>
-    <GoogleOAuthProvider clientId={googleClientId}> {/* <-- 3. Avvolgi tutto */}
+    
       <BrowserRouter>
         <AuthProvider>
           <App />
         </AuthProvider>
       </BrowserRouter>
-    </GoogleOAuthProvider>
+    
   </React.StrictMode>
 );
 
